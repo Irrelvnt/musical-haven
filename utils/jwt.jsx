c90785ba7486ee6738a4496e0f8fa4ff71a75208
aymane <aymanereventon@gmail.com>
2023-03-19 22:45:10 +0000

    domain

--- utils/jwt.jsx
+++ utils/jwt.jsx
@@ -42,7 +42,7 @@ const attachCookiesToResponse = ({ req, res, user, refreshToken }) => {
     setCookie("accessToken", accessTokenJWT, {
       req,
       res,
-      domain: ".vercel.app",
+      domain: "musical-haven.vercel.app",
       httpOnly: true,
       sameSite: true,
       secure: true,
@@ -52,7 +52,7 @@ const attachCookiesToResponse = ({ req, res, user, refreshToken }) => {
     setCookie("refreshToken", refreshTokenJWT, {
       req,
       res,
-      domain: ".vercel.app",
+      domain: "musical-haven.vercel.app",
       httpOnly: true,
       sameSite: true,
       secure: true,
